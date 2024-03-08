427827f2b752bc925622be6ac0bb1ec811eea6fb
sebarodriguezm <rodriguezmunoz.seba@gmail.com>
2024-03-08 13:45:05 -0300

    se cargan de a 25

--- src/app/pages/pokemon-list/pokemon-list.page.ts
+++ src/app/pages/pokemon-list/pokemon-list.page.ts
@@ -9,22 +9,33 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 })
 export class PokemonListPage implements OnInit {
   pokemonList: any[] = [];
-
+ offset: number = 0;
   constructor(private pokeapi: PokemonService) {}
 
   ngOnInit() {
     this.getPokemons();
   }
-
-  getPokemons(): void {
+                // obtener pokemons
+  getPokemons(offset: number = 0, limit: number = 25): void {
     const requests = [];
-    for (let i = 1; i <= 151; i++) {
+    for (let i = offset + 1; i <= offset + limit; i++) {
       requests.push(this.pokeapi.getPokemon(i));
     }
-
+    // se usa forkjoin para que se carguen todos en paralelo y luego se guarda todo en pokemonList
     forkJoin(requests).subscribe((data: any[]) => {
-      this.pokemonList = data;
+      this.pokemonList = this.pokemonList.concat(data);
       console.log(this.pokemonList);
     });
   }
+  
+  loadMorePokemons(event:any): void {
+    this.offset += 25; // Incrementar el offset para obtener el siguiente lote de Pokémon
+    this.getPokemons(this.offset); // Cargar más Pokémon con el nuevo offset
+
+    // Finalizar el evento de carga infinita
+    setTimeout(() => {
+      event.target.complete();
+    }, 500);
+  }
+  
 }
