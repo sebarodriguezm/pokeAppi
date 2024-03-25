d98c32e5cf266bb242f854aa235aa9f321df02cf
sebarodriguezm <rodriguezmunoz.seba@gmail.com>
2024-03-24 23:41:19 -0300

    ajuste const baseurl

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,32 +1,29 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
+  private baseUrl = 'https://pokeapi.co/api/v2/';
+
   constructor(private http: HttpClient) { }
 
   getPokemon(id: number): Observable<any> {
-    return this.http.get<any>(`${environment.pokemonApi.baseUrl}${id}`);
+    return this.http.get<any>(`${this.baseUrl}pokemon/${id}`);
   }
 
   getPokemonSpecies(id: number): Observable<any> {
-    return this.http.get<any>(`${environment.pokemonApi.baseUrl}pokemon-species/${id}`);
+    return this.http.get<any>(`${this.baseUrl}pokemon-species/${id}`);
   }
-  
 
   getEvolutionChain(id: number): Observable<any> {
-    return this.http.get<any>(`https://pokeapi.co/api/v2/evolution-chain/${id}`);
+    return this.http.get<any>(`${this.baseUrl}evolution-chain/${id}`);
   }
 
   getPokemonTypes(): Observable<any> {
-    return this.http.get<any>('https://pokeapi.co/api/v2/type');
+    return this.http.get<any>(`${this.baseUrl}type`);
   }
-
-
 }
